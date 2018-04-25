0ca890f036e83fc74e73ad138a72e214e6fd6661
tati-micheletti <tati.micheletti@gmail.com>
2018-04-24 21:54:16 -0700

    scfmCrop.R updated

--- modules/scfmCrop/scfmCrop.R
+++ modules/scfmCrop/scfmCrop.R
@@ -23,12 +23,21 @@ defineModule(sim, list(
   inputObjects=bind_rows(
     expectsInput(objectName = "studyArea", objectClass = "SpatialPolygons", desc = "study area"),
     expectsInput(objectName = "vegMapInit", objectClass = "RasterLayer", desc = "national landcover map LCC05"),
-    expectsInput(objectName = "ageMapInit", objectClass ="RasterLayer", desc = "national 1km2 ageMap")
+    expectsInput(objectName = "ageMapInit", objectClass ="RasterLayer", desc = "national 1km2 ageMap"),
+    expectsInput(objectName = "url.studyArea", objectClass = "character", desc = NA, sourceURL = NA),
+    expectsInput(objectName = "url.vegMap", objectClass = "character", desc = NA, sourceURL = NA),
+    expectsInput(objectName = "tempPath.studyArea", objectClass = "character", desc = "Temporary path to downloaded study area", sourceURL = NA),
+    expectsInput(objectName = "tempPath.vegMap", objectClass = "character", desc = "Temporary path to downloaded vegetation map", sourceURL = NA),
+    expectsInput(objectName = "numTypes", objectClass = "numeric", desc = "Number of strata", sourceURL = NA)
+    
   ),
   outputObjects=bind_rows(
     createsOutput(objectName = "vegMap", objectClass = "RasterLayer", desc = ""),
     createsOutput(objectName = "ageMap", objectClass = "RasterLayer", desc = ""),
-    createsOutput(objectName = "Mask", objectClass = "RasterLayer", desc = "")
+    createsOutput(objectName = "studyArea", objectClass = "shapefile", desc = ""),
+    createsOutput(objectName = "templateRaster", objectClass = "RasterLayer", desc = ""),
+    createsOutput(objectName = "strataMap", objectClass = "shapefile", desc = "Number of strata"),
+    createsOutput(objectName = "flammableMap", objectClass = "RasterLayer", desc = "")
   )
 ))
 
@@ -45,36 +54,36 @@ doEvent.scfmCrop = function(sim, eventTime, eventType, debug=FALSE) {
 
 Init <- function(sim) {
 
-  vegProjection <- crs(sim$vegMapInit)
-  
-  if (is.na(crs(sim$studyArea)))            #in case it was sampled from the vegmap.
-    crs(sim$studyArea) <- vegProjection
-  
-  simProjection <- crs(sim$studyArea)         #this would probably be set to be the same as the veg map at an earlier stage.
-  
-  #if(ncell(sim$vegMap)>5e5) beginCluster(min(parallel::detectCores(),6))
-  
-  #Project the study area into each input raster, then crop and mask; 
-  #Then project result back into the sim projection.
-  #browser()
-  studyAreaTmp <- spTransform(sim$studyArea, CRSobj = vegProjection)
-  sim$vegMap <-  crop(sim$vegMapInit, studyAreaTmp)
-  crs(sim$vegMap) <- vegProjection
-  sim$vegMap <- mask(sim$vegMap, studyAreaTmp) #É
-  #sim$vegMap <- projectRaster(sim$vegMap, crs=simProjection, method="ngb", to=sim$vegMapInit)
-  sim$Mask <- sim$vegMap
-  sim$Mask[] <- ifelse(is.na(sim$vegMap[]), NA, 1)
-  
-  tmp <- getColors(sim$vegMapInit)[[1]]        # mask removes colors!
-  setColors(sim$vegMap, n=length(tmp)) <- tmp  # so put them back.
-  
-  ageProjection <- crs(sim$ageMapInit)
-  studyAreaTmp <- spTransform(sim$studyArea, CRSobj =ageProjection)
-  sim$ageMap <-  crop(sim$ageMapInit, studyAreaTmp)
- # crs(sim$ageMap) <- ageProjection
-  sim$ageMap <- mask(sim$ageMap,studyAreaTmp)
-  sim$ageMap <- projectRaster(sim$ageMap,to=sim$vegMap,method="ngb")
-  
+ #  vegProjection <- crs(sim$vegMapInit)
+ #  
+ #  if (is.na(crs(sim$studyArea)))            #in case it was sampled from the vegmap.
+ #    crs(sim$studyArea) <- vegProjection
+ #  
+ #  simProjection <- crs(sim$studyArea)         #this would probably be set to be the same as the veg map at an earlier stage.
+ #  
+ #  #if(ncell(sim$vegMap)>5e5) beginCluster(min(parallel::detectCores(),6))
+ #  
+ #  #Project the study area into each input raster, then crop and mask; 
+ #  #Then project result back into the sim projection.
+ #  #browser()
+ #  studyAreaTmp <- spTransform(sim$studyArea, CRSobj = vegProjection)
+ #  sim$vegMap <-  crop(sim$vegMapInit, studyAreaTmp)
+ #  crs(sim$vegMap) <- vegProjection
+ #  sim$vegMap <- mask(sim$vegMap, studyAreaTmp) #É
+ #  #sim$vegMap <- projectRaster(sim$vegMap, crs=simProjection, method="ngb", to=sim$vegMapInit)
+ #  sim$Mask <- sim$vegMap
+ #  sim$Mask[] <- ifelse(is.na(sim$vegMap[]), NA, 1)
+ #  
+ #  tmp <- getColors(sim$vegMapInit)[[1]]        # mask removes colors!
+ #  setColors(sim$vegMap, n=length(tmp)) <- tmp  # so put them back.
+ #  
+ #  ageProjection <- crs(sim$ageMapInit)
+ #  studyAreaTmp <- spTransform(sim$studyArea, CRSobj =ageProjection)
+ #  sim$ageMap <-  crop(sim$ageMapInit, studyAreaTmp)
+ # # crs(sim$ageMap) <- ageProjection
+ #  sim$ageMap <- mask(sim$ageMap,studyAreaTmp)
+ #  sim$ageMap <- projectRaster(sim$ageMap,to=sim$vegMap,method="ngb")
+ #  
   #endCluster()
     
   if(sum(!is.na(getValues(sim$ageMap)))==0)
@@ -87,18 +96,71 @@ Init <- function(sim) {
 
 .inputObjects <- function(sim){
   
-  shape <- readOGR(dsn = file.path(inputPath(sim), "ecodistricts"), layer = "ecodistricts") # ADDED TATI
-  sim$studyArea <- shape[shape$FID==339,] # ADDED TATI
+  sim$templateRaster <- Cache(prepInputs, url = sim$url.vegMap,
+                              destinationPath = asPath(sim$tempPath.vegMap))
+  
+  # shape <- readOGR(dsn = file.path(inputPath(sim), "ecodistricts"), layer = "ecodistricts") # ADDED TATI
+  # sim$studyArea <- shape[shape$FID==339,] # ADDED TATI
+
+  if(!file.exists(file.path(inputPath(sim), "studyArea.shp"))){
+    sim$studyArea <- Cache(prepInputs, url = sim$url.studyArea,
+                           destinationPath = sim$tempPath.studyArea,
+                           rasterToMatch = sim$templateRaster) 
+    sim$studyArea <- sim$studyArea[sim$studyArea$ECODISTRIC==339,]
     
-  if (!("ageMapInit" %in% sim$.userSuppliedObjNames)){
-    fp <- file.path(dataPath(sim),"ageMapInit","age.tif")
-    sim$ageMapInit <- raster(fp)
+    rgdal::writeOGR(obj = sim$studyArea, dsn = file.path(inputPath(sim), "studyArea.shp"), layer = "studyArea.shp", driver = "ESRI Shapefile")
+    
+  } else {
+    
+    sim$studyArea <- Cache(prepInputs, 
+                           targetFile = asPath(file.path(inputPath(sim), "studyArea.shp")),
+                           destinationPath = asPath(file.path(inputPath(sim))),
+                           rasterToMatch = sim$templateRaster)
+    
+    studyAreaDF <- rgdal::readOGR(dsn = file.path(inputPath(sim),"studyArea.shp"))
+    
+  }
+
+  sim$vegMap <- Cache(raster::crop, sim$templateRaster, sim$studyArea)
+  sim$vegMap <- Cache(fastMask, sim$vegMap, studyAreaDF) # if not needed, comment lines pid, pdf
+
+  if (!suppliedElsewhere("vegMapInit",sim)){
+    suppliedElsewhere("vegMap",sim)
+      sim$vegMapInit <- sim$vegMap
+    } else stop("VegMap not supplied an/or not available online")
+  
+  if (!suppliedElsewhere("ageMap",sim)){
+    
+    sA <- if(suppliedElsewhere(sim$studyArea)) SpatialPolygons(sim$studyArea@polygons,proj4string=sim$studyArea@proj4string) else NULL
+    sADF <- if(suppliedElsewhere(sim$studyArea)) sim$studyArea else NULL
+    rTm <- if(suppliedElsewhere(sim$vegMap)) sim$vegMap else NULL
+
+    browser() #Error in match.arg(algo) : 'arg' must be NULL or a character vector
+    
+    tryCatch({sim$ageMap <- prepInputs(targetFile = asPath(file.path(dataPath(sim), "can_age04_1km.tif")),
+                                      destinationPath = asPath(file.path(dataPath(sim))),
+                                      studyArea = sADF,
+                                      rasterToMatch = rTm)},
+             finally = {sim$ageMap <- raster(raster::extent(0,49,0,49),nrow=200, ncol=200, vals=as.integer(runif(200*200)*150))
+             warning("Age map was not supplied, creating a random raster.")})
+    }
+  
+  if (!suppliedElsewhere("ageMapInit",sim)){
+    suppliedElsewhere("ageMap",sim)
+    sim$ageMapInit <- sim$ageMap
+  } else stop("ageMap not supplied, and ageMapInit failed to be created.")
+  
+  if (suppliedElsewhere("ageMap", sim)){
+    sim$flammableMap <- sim$ageMap * 0   #this, on the other hand, had better exist
+    #    sim$ageMap[] <- P(sim)$initialAge ===> Not sure what this does 
+  } 
+  else {
+    
+    sim$flammableMap <- raster(raster::extent(0,49,0,49),nrow=200, ncol=200, vals=0)
+    warning("Age map was not supplied, creating a random raster.")
   }
   
-  if (!("vegMapInit" %in% sim$.userSuppliedObjNames)){
-    fp <- file.path(dataPath(sim),"vegMapInit","LCC2005_V1_4a.tif")
-    sim$vegMapInit <- raster(fp)
-  }
+  sim$strataMap <- randomPolygons(sim$ageMap,sim$numTypes)
   
   return(invisible(sim))
 }
