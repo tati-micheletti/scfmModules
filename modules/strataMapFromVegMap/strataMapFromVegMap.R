55f0f4a55743b166487f875d003eb8a84c40579b
tati-micheletti <tati.micheletti@gmail.com>
2018-04-27 16:03:24 -0700

    updated strataMapFromVegMap (Caching)

--- modules/strataMapFromVegMap/strataMapFromVegMap.R
+++ modules/strataMapFromVegMap/strataMapFromVegMap.R
@@ -18,7 +18,7 @@ defineModule(sim, list(
     defineParameter(".initialTime", "numeric", 0, NA, NA, "This describes the simulation time interval between plot events"),
     defineParameter(".saveInitialTime", "numeric", NA, NA, NA, "This describes the simulation time at which the first save event should occur"),
     defineParameter(".saveInterval", "numeric", NA, NA, NA, "This describes the simulation time interval between save events"),
-    defineParameter(".useCache", "logical", FALSE, NA, NA, "Should this entire module be run with caching activated? This is generally intended for data-type modules, where stochasticity and time are not relevant")
+    defineParameter(".useCache", "logical", TRUE, NA, NA, "Should this entire module be run with caching activated? This is generally intended for data-type modules, where stochasticity and time are not relevant")
   ),
   inputObjects = bind_rows(
     #expectsInput("objectName", "objectClass", "input object description", sourceURL, ...),
