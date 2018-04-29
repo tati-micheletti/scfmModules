ebcddbecbdd552e2831a5f27ff4e47dc89275c6f
tati-micheletti <tati.micheletti@gmail.com>
2018-04-28 22:36:16 -0700

    efficient getNeighborhoodCovars

--- modules/birdsAlberta/R/getNeighborhoodCovars.R
+++ modules/birdsAlberta/R/getNeighborhoodCovars.R
@@ -5,91 +5,58 @@ getNeighborhoodCovars <- function(covarTable = sim$covar,
                                   habitatMap = sim$habitatMap,
                                   vegMap = sim$vegMap){
   
-  
-   browser()
-  
-  #ROUND NEIGHBORHOOD
-  # distMatrix <- matrix(c(0, rep(1, 3), 0, rep(1, 7), 0, rep(1, 7), 0, rep(1, 3), 0), ncol = 5) #Doesn't work for adjacent
-  
   distMatrix <- matrix(c(rep(1, 12), 0, rep(1,12)), ncol = 5)
   
-  cellsN <- data.table::data.table(adjacent(habitatMap, 
-                                              cells = c(1:ncell(habitatMap)), 
-                                              directions = distMatrix, 
-                                              include = FALSE, 
-                                              pairs = TRUE, 
-                                              id = FALSE))
-  # NUMERATORS
-  extrHabitat <- habitatMap[cellsN$to] #Not right order?
+  cellsN <- data.table::data.table(raster::adjacent(habitatMap, 
+                                                    cells = c(1:ncell(habitatMap)), 
+                                                    directions = distMatrix, 
+                                                    include = FALSE, 
+                                                    pairs = TRUE, 
+                                                    id = FALSE))
+  
+  extrHabitat <- habitatMap[cellsN$to]
   extrAge <- ageMap[cellsN$to]
   extrDisturbance <- disturbanceMap[cellsN$to]
   
   cellsN$habitat <- extrHabitat
   cellsN$age <- extrAge
   cellsN$disturbance <- extrDisturbance
-    
+  
   countPerNeig <- cellsN[, .N, by = from] # Summary Table
   
-  # actab <- tabulate(distMatrix)
-  # actab <- tabulate(ac[,2]/24)
+  cropMap <- vegMap[]*0
+  cropMap[] <- ifelse(cropMap[]==0, 1, NA)
   
-  N_LATE <- sum(ageMap[ac[,1]]>90)/24
-  N_RICH <- sum(actab>0)
+  N_CUT <-  cellsN[, sum(disturbance==2 | age<31, na.rm = TRUE), by = from]
+  N_CUT <- N_CUT$V1/countPerNeig$N
+  N_CUT <- N_CUT*cropMap
   
+  N_LATE <-  cellsN[, sum(disturbance==0 & age>90 & habitat==5, na.rm = TRUE), by = from]
+  N_LATE <- N_LATE$V1/countPerNeig$N
+  N_LATE <- N_LATE*cropMap
+  
+  N_DEC <-  cellsN[, sum(disturbance==0 & (habitat==4 | habitat==5), na.rm = TRUE), by = from]
+  N_DEC <- N_DEC$V1/countPerNeig$N
+  N_DEC <- N_DEC*cropMap
+  
+  N_MIX <-  cellsN[, sum(disturbance==0 & habitat==9, na.rm = TRUE), by = from]
+  N_MIX <- N_MIX$V1/countPerNeig$N
+  N_MIX <- N_MIX*cropMap
+  
+  N_SB <-  cellsN[, sum(disturbance==0 & habitat==7, na.rm = TRUE), by = from]
+  N_SB <- ifelse(N_SB$V1>0,1,0)
+  N_SB <- N_SB*cropMap
+  
+  N_RICH <-  cellsN[, length(unique(habitat)), by = from]
+  N_RICH <- N_RICH$V1
+  N_RICH <- N_RICH*cropMap
+  
+  covarTable$N_CUT <- N_CUT
+  covarTable$N_DEC <- N_DEC
+  covarTable$N_MIX <- N_MIX
+  covarTable$N_LATE <- N_LATE
+  covarTable$N_SB <- N_SB
+  covarTable$N_RICH <- N_RICH
 
-  # 
-  # 
-  # # TO SUBSTITUTE IN COVAR TABLE
-  # 
-  # invisible({
-  #   
-  #   if(!"ID" %in% names(covarTable)){
-  #     covarTable$ID <- seq(1:nrow(covarTable))
-  #   }
-  #   naCells <- which(is.na(habitatMap[]))
-  #   covarTable <- covarTable[ID %in% naCells, names(covarTable)[1:12] := NA_real_]
-  #   
-  #   # L_CUT
-  #   if(!length(which(disturbanceMap[]==2 | ageMap[]<31))==0){
-  #     L_CUT.rows <- which(disturbanceMap[]==2 | ageMap[]<31)
-  #     covarTable[ID %in% L_CUT.rows, L_CUT := 1L]
-  #   }
-  #   
-  #   # L_ODEC
-  #   if(!length(which(disturbanceMap[]==0 & ageMap[]>90 & habitatMap[]==5))==0){
-  #     L_ODEC.rows <- which(disturbanceMap[]==0 & ageMap[]>90 & habitatMap[]==5) 
-  #     covarTable[ID %in% L_ODEC.rows, L_ODEC := 1L]  
-  #   }
-  #   
-  #   # L_MIX (old)
-  #   if(!length(which(disturbanceMap[]==0 & ageMap[]>90 & habitatMap[]==9))==0){
-  #     L_MIX.rows <- which(disturbanceMap[]==0 & ageMap[]>90 & habitatMap[]==9)
-  #     covarTable[ID %in% L_MIX.rows, L_MIX := 1L]
-  #   }
-  #   
-  #   # L_YDEC
-  #   if(!length(which(disturbanceMap[]==0 & (ageMap[]>30 & ageMap[]<91) & habitatMap[]==4))==0){
-  #     L_YDEC.rows <- which(disturbanceMap[]==0 & (ageMap[]>30 & ageMap[]<91) & habitatMap[]==4)
-  #     covarTable[ID %in% L_YDEC.rows, L_YDEC := 1L]  
-  #   }
-  #   
-  #   # L_MIX (young)
-  #   if(!length(which(disturbanceMap[]==0 & (ageMap[]>30 & ageMap[]<91) & habitatMap[]==9))==0){
-  #     L_MIX.rows <- which(disturbanceMap[]==0 & (ageMap[]>30 & ageMap[]<91) & habitatMap[]==9)
-  #     covarTable[ID %in% L_MIX.rows, L_MIX := 1L]
-  #   }
-  #   
-  #   # L_CC
-  #   closed <- which(vegMap[] %in% c(1:5))
-  #   medium <- which(vegMap[] %in% c(6:7, 12))
-  #   low <- which(vegMap[] %in% c(8:11, 13:15, 25))
-  #   covarTable[ID %in% closed, L_CC := 0.8]
-  #   covarTable[ID %in% medium, L_CC := 0.6]
-  #   covarTable[ID %in% low, L_CC := 0.3]
-  #   
-  #   #L_WDIS: already 0
-  #   
-  # })
-  # 
   return(invisible(covarTable))
 }
