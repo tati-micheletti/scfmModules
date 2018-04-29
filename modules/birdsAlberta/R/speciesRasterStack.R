01e766cdcfc3c68dfb1c9049e2d4c0a3dd1c8500
tati-micheletti <tati.micheletti@gmail.com>
2018-04-28 22:35:24 -0700

    SpeciesRasterStack() :: birdsAlberta module

---
+++ modules/birdsAlberta/R/speciesRasterStack.R
@@ -0,0 +1,17 @@
+speciesRasterStack <- function(birdModelVernier = sim$birdModelVernier,
+                               rasterTemplate = sim$vegMap){
+ 
+  species <- names(birdModelVernier)
+
+  rasterStack <- lapply(species, FUN = function(species){
+    
+    rasterTemplate[] <- birdModelVernier[[species]]
+
+    return(rasterTemplate)
+  })
+  
+  names(rasterStack) <- species
+  rasterStack <- raster::stack(rasterStack)
+
+  return(rasterStack)
+}
