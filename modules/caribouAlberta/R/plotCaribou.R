6bf491dea154db32a096ebc03140870292ab1f70
Eliot McIntire <eliotmcintire@gmail.com>
2018-07-27 14:59:18 -0700

    Eliot tweaks

--- modules/caribouAlberta/R/plotCaribou.R
+++ modules/caribouAlberta/R/plotCaribou.R
@@ -1,9 +1,9 @@
 plotCaribou <- function(sim = sim, SorensenStats = sim$SorensenStats){
 
   
-populationCaribou <- data.frame(Time = 0:time(sim), N = SorensenStats[,1])
+populationCaribou <- data.frame(Time = 0:time(sim), lambda = SorensenStats[,1])
 
-plotsCarib <- ggplot2::ggplot(data = populationCaribou, aes(x = Time, y = N)) +
+plotsCarib <- ggplot2::ggplot(data = populationCaribou, aes(x = Time, y = lambda)) +
                       geom_line()
 
 # growthCaribou <- data.frame(Time = 1:time(sim), lambda = SorensenStats[,1])
